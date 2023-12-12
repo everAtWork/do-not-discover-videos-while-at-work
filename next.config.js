d287dba7cdc59329b2d1b46400194c675418679c
jalsaenggin <59497788+everAtWork@users.noreply.github.com>
2023-12-12 14:48:51 +0200

    Popular Videos API Docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	env: {
+		YOUTUBE_API_KEY: "AIzaSyBey0Nvh_5CQAYHJOzI8IAVRU2qEKMBmVY",
+	},
 	swcMinify: true,
 	images: {
 		domains: ["images.unsplash.com", "i.ytimg.com"],
