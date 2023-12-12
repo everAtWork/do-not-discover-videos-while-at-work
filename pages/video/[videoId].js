344b6707886172baca15bc91d0fcdfc723249293
jalsaenggin <59497788+everAtWork@users.noreply.github.com>
2023-12-12 18:53:02 +0200

    Link Dynamic Page to Card Component Routing

---
+++ pages/video/[videoId].js
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router";
+const Video = () => {
+	const router = useRouter();
+	console.log({ router });
+	return <div>video page {router.query.videoId}</div>;
+};
+export default Video;
