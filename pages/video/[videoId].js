e0f2d9ff38d8f6d28615b18cd218a89555a07c8d
jalsaenggin <59497788+everAtWork@users.noreply.github.com>
2024-02-20 12:43:23 +0100

    [Stats API] useHook for videoID.js plus others

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
@@ -49,6 +49,24 @@ const Video = ({ video }) => {
 		channelTitle,
 		statistics: { viewCount } = { viewCount: 0 },
 	} = video;
+	useEffect(() => {
+		const handleLikeDislikeService = async () => {
+			const response = await fetch(`/api/stats?videoId=${videoId}`, {
+				method: "GET",
+			});
+			const data = await response.json();
+
+			if (data.length > 0) {
+				const favourited = data[0].favourited;
+				if (favourited === 1) {
+					setToggleLike(true);
+				} else if (favourited === 0) {
+					setToggleDisLike(true);
+				}
+			}
+		};
+		handleLikeDislikeService();
+	}, [videoId]);
 
 	const runRatingService = async (favourited) => {
 		return await fetch("/api/stats", {
