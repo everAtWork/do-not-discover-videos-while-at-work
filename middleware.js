973691ba3fafa66cbad8ed1f8f78af97686b790d
jalsaenggin <59497788+everAtWork@users.noreply.github.com>
2024-03-05 10:58:49 +0100

    [Middleware] Middleware Implementation

--- middleware.js
+++ middleware.js
@@ -1,12 +1,22 @@
 import { NextResponse } from "next/server";
+import { verifyToken } from "./lib/utils";
 
-export function middleware(req, ev) {
-	console.log({ req, ev });
-	//check the token
-	//if token is valid
-	//|| if page is /login
-	return NextResponse.next();
+export async function middleware(req, ev) {
+	const token = req ? req.cookies.get("token") : null;
+	const userId = await verifyToken(token);
+	const { pathname } = req.nextUrl;
 
-	//if no token
-	//redirect to login
+	if (
+		pathname.includes("/api/login") ||
+		userId ||
+		pathname.includes("/static")
+	) {
+		return NextResponse.next();
+	}
+
+	if ((!token || !userId) && pathname !== "/login") {
+		const url = req.nextUrl.clone();
+		url.pathname = "/login";
+		return NextResponse.rewrite(url);
+	}
 }
